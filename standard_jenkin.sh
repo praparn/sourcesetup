abab0be0942aa513433b0447d73c2ded331c6a5a
praparn <eva10409@gmail.com>
2018-06-11 09:25:28 +0700

    20180611092525

--- standard_jenkin.sh
+++ standard_jenkin.sh
@@ -25,6 +25,7 @@ echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
 #Install Jenkins
 sudo apt-get install -y default-jre
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 sudo apt-get update
 sudo apt-get install -y jenkins
 sudo systemctl start jenkins
