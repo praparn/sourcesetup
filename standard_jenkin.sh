1dc78669400283655af44c0424a8fda3eb81eb21
praparn <eva10409@gmail.com>
2018-06-11 09:35:47 +0700

    20180611093545

--- standard_jenkin.sh
+++ standard_jenkin.sh
@@ -27,6 +27,7 @@ sudo apt-get install -y default-jre
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
 sudo apt-get update
+sleep 5s
 sudo apt-get install -y jenkins
 sudo systemctl start jenkins
 #restart
