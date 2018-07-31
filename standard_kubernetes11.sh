a6da6230198193a4d2fe84bf60bb6e6f700c321d
praparn <eva10409@gmail.com>
2018-07-31 15:59:05 +0700

    20180731155900

--- standard_kubernetes11.sh
+++ standard_kubernetes11.sh
@@ -48,9 +48,9 @@ usermod -a -G docker 1001
 sudo apt-get update && sudo apt-get install -y apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo touch /etc/apt/sources.list.d/kubernetes.list 
-sudo bash -c echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubectl kubelet kubeadm kubectl kubernetes-cni
+sudo apt-get install -y kubectl=1.11.0-00 kubelet=1.11.0-00 kubeadm=1.11.0-00 kubectl=1.11.0-00 kubernetes-cni
 
 #restart
 #reboot
