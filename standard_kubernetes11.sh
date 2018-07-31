77b1468d12e2550e91e1f072104ae89e0e673bee
praparn <eva10409@gmail.com>
2018-07-31 15:24:34 +0700

    20180731152431

---
+++ standard_kubernetes11.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+#Set LC_ALL, LANG
+echo "LC_ALL=en_US.UTF-8" >> /etc/default/locate
+echo "LANG=en_US.UTF-8" >> /etc/default/locate
+
+#tuning sysctl.conf
+echo "net.core.rmem_max = 16777216" >> /etc/sysctl.conf
+echo "net.core.wmem_max = 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_rmem = 4096 87380 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_wmem = 4096 65536 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_probes = 9" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_time = 7200" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syn_retries = 5" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_no_metrics_save = 1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_synack_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syn_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_tw_reuse=1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_fin_timeout=15" >> /etc/sysctl.conf
+echo "net.ipv4.ip_local_port_range = 2000 65000" >> /etc/sysctl.conf
+echo "fs.file-max = 1000000" >> /etc/sysctl.conf
+echo "vm.swappiness = 0" >> /etc/sysctl.conf
+echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+
+#create 1001 user
+useradd -u 1001 --no-create-home 1001
+mkdir -p /var/www && sudo chown 1001:1001 /var/www
+mkdir -p /var/dockers && sudo chown 1001:1001 /var/dockers
+
+#install docker
+apt-get update
+apt-get install -y curl
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-cache policy docker-ce
+apt-get install -y docker-ce
+
+#install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#add ubuntu and 1001 to docker group
+usermod -a -G docker ubuntu
+usermod -a -G docker 1001
+
+#Install Kubernetes Base
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list 
+sudo bash -c echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl=1.11.0-00 kubelet=1.11.0-00 kubeadm=1.11.0-00 kubectl=1.11.0-00 kubernetes-cni=1.11.0-00
+
+#restart
+#reboot
