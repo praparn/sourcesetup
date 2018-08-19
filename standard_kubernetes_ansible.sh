0119db2f041a93017f31c2f8cc3216a2f3b5cd7a
praparn <eva10409@gmail.com>
2018-08-19 07:50:49 +0700

    20180819075047

---
+++ standard_kubernetes_ansible.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#Set LC_ALL, LANG
+echo "LC_ALL=en_US.UTF-8" >> /etc/default/locate
+echo "LANG=en_US.UTF-8" >> /etc/default/locate
+
+#tuning sysctl.conf
+echo "net.core.rmem_max = 16777216" >> /etc/sysctl.conf
+echo "net.core.wmem_max = 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_rmem = 4096 87380 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_wmem = 4096 65536 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_probes = 9" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_time = 7200" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syn_retries = 5" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_no_metrics_save = 1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_synack_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syn_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_tw_reuse=1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_fin_timeout=15" >> /etc/sysctl.conf
+echo "net.ipv4.ip_local_port_range = 2000 65000" >> /etc/sysctl.conf
+echo "fs.file-max = 1000000" >> /etc/sysctl.conf
+echo "vm.swappiness = 0" >> /etc/sysctl.conf
+echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+
+#install ansible
+apt-get update && apt-get install software-properties-common && apt-add-repository -y ppa:ansible/ansible && apt-get update && apt-get install -y ansible
+swapoff -a
+#restart
+#reboot
