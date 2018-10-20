52eb3d2622136f07d52fcdfc9555aa584f5f1e8a
praparn <eva10409@gmail.com>
2018-10-20 10:25:17 +0700

    20181020102514

--- standard_docker_old.sh
+++ standard_docker_old.sh
@@ -37,7 +37,7 @@ apt-cache policy docker-ce
 apt-get install -y docker-ce
 
 #install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 #add ubuntu and 1001 to docker group
