1ce6f5dd6365ccc53c2056c7a5d3934e70d4db5e
praparn <eva10409@gmail.com>
2018-11-20 00:39:44 +0700

    20181120003942

--- standard_kuberneteslatest_aws.sh
+++ standard_kuberneteslatest_aws.sh
@@ -39,7 +39,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-cache policy docker-ce
-apt-get install -y docker-ce
+apt-get install -y docker-ce=17.03.3~ce-0~ubuntu-xenial
 
 #install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
