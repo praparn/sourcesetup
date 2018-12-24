28c044930c7c190c6fadf2e3e6575d2f91250b73
praparn <eva10409@gmail.com>
2018-12-24 21:23:42 +0700

    20181224212339

--- standard_docker_aws.sh
+++ standard_docker_aws.sh
@@ -21,11 +21,31 @@ echo "net.ipv4.ip_local_port_range = 2000 65000" >> /etc/sysctl.conf
 echo "fs.file-max = 1000000" >> /etc/sysctl.conf
 echo "vm.swappiness = 0" >> /etc/sysctl.conf
 echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+#tuning limits.conf
+echo "* soft    nproc    65535" >> /etc/security/limits.conf
+echo "* hard    nproc    65535" >> /etc/security/limits.conf
+echo "* soft    nofile   65535" >> /etc/security/limits.conf
+echo "* hard    nofile   65535" >> /etc/security/limits.conf
+echo "root soft    nproc    65535" >> /etc/security/limits.conf  
+echo "root hard    nproc    65535" >> /etc/security/limits.conf
+echo "root soft    nofile   65535" >> /etc/security/limits.conf
+echo "root hard    nofile   65535" >> /etc/security/limits.conf
 
 #install NTP
 wget https://raw.githubusercontent.com/praparn/sourcesetup/master/standard_ntp.sh
 sudo chmod +x ./standard_ntp.sh
 ./standard_ntp.sh
+
+#configure monitor memory on host
+apt-get update && apt-get install -y unzip libwww-perl libdatetime-perl
+mkdir /home/ubuntu/MonitoringMemoryandDisk
+cd /home/ubuntu/MonitoringMemoryandDisk
+curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
+unzip CloudWatchMonitoringScripts-1.2.2.zip && \
+rm CloudWatchMonitoringScripts-1.2.2.zip
+
+#add cronjob
+echo "*/5 * * * * /home/ubuntu/MonitoringMemoryandDisk/aws-scripts-mon/mon-put-instance-data.pl --mem-used-incl-cache-buff --mem-util --mem-used --mem-avail --disk-space-util --disk-path=/ --from-cron" | crontab -
 
 #create 1001 user
 groupadd docker
