edcf85ec42b93dedb092181b52c11d2ccfcd6b27
praparn <eva10409@gmail.com>
2019-01-19 10:27:07 +0700

    20190119102705

--- standard_jmeter_ubuntu.sh
+++ standard_jmeter_ubuntu.sh
@@ -37,7 +37,7 @@ sudo chmod +x ./standard_ntp.sh
 ./standard_ntp.sh
 
 #Install Base Binary File and Initial Configuration on UltraVNC
-apt update && apt install -y \
+sudo apt update && apt install -y \
   ubuntu-desktop \
   vnc4server \
   gnome-panel \
@@ -47,5 +47,13 @@ apt update && apt install -y \
   gnome-terminal \
   jmeter
 
+sudo mkdir /home/ubuntu/.vnc
+curl https://raw.githubusercontent.com/praparn/sourcesetup/master/xstartup > /home/ubuntu/.vnc/xstartup
+
+#Install Jmeter Binary and Lastest File
+
+curl https://www-us.apache.org/dist//jmeter/binaries/apache-jmeter-5.0.tgz > /home/ubuntu/apache-jmeter-5.0.tgz
+  tar -xf /home/ubuntu/apache-jmeter-5.0.tgz
+  cd apache-jmeter-5.0/bin/
 #restart
 #reboot
