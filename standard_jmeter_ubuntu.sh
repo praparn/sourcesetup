802714ec4e9cee75dbac37a6bb44ca4f8753734e
praparn <eva10409@gmail.com>
2019-01-20 21:43:25 +0700

    20190120214322

--- standard_jmeter_ubuntu.sh
+++ standard_jmeter_ubuntu.sh
@@ -8,14 +8,3 @@ sudo apt update && apt install -y \
   nautilus \
   gnome-terminal \
   jmeter
-
-sudo mkdir /home/ubuntu/.vnc
-sudo curl https://raw.githubusercontent.com/praparn/sourcesetup/master/xstartup > /home/ubuntu/.vnc/xstartup
-
-#Install Jmeter Binary and Lastest File
-
-sudo curl https://www-us.apache.org/dist//jmeter/binaries/apache-jmeter-5.0.tgz > /home/ubuntu/apache-jmeter-5.0.tgz
-sudo tar -xf /home/ubuntu/apache-jmeter-5.0.tgz
-cd apache-jmeter-5.0/bin/
-#restart
-#reboot
