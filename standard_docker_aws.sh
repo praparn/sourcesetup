d1df04377d7f641738a9117ac482fa7acfd21791
praparn <eva10409@gmail.com>
2019-03-02 20:26:53 +0700

    20190302202651

--- standard_docker_aws.sh
+++ standard_docker_aws.sh
@@ -54,17 +54,23 @@ mkdir -p /var/www && sudo chown 1001:1001 /var/www
 mkdir -p /var/dockers && sudo chown 1001:1001 /var/dockers
 
 #install docker
-apt-get update
-apt-get install -y curl
+apt-get update && apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common && 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-apt-cache policy docker-ce
-apt-get install -y docker-ce
+apt-get install docker-ce docker-ce-cli containerd.io
 
 #install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+#add ubuntu and 1001 to docker group
+usermod -a -G docker ubuntu
+usermod -a -G docker 1001
 
 #add ubuntu and 1001 to docker group
 usermod -a -G docker ubuntu
