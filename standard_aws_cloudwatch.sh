f9383ed524a3e824b4f714ba2fa1409629acb24f
praparn <eva10409@gmail.com>
2019-09-06 17:09:08 +0700

    Update script cloudwatch

--- standard_aws_cloudwatch.sh
+++ standard_aws_cloudwatch.sh
@@ -2,6 +2,6 @@
 sudo apt update -y 
 sudo apt install wget -y
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-wget https://wichawit-sample.s3-ap-northeast-1.amazonaws.com/linux-cwagentlog.json
+wget https://raw.githubusercontent.com/praparn/sourcesetup/master/linux-cwagentlog.json
 sudo dpkg -i ./amazon-cloudwatch-agent.deb
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:linux-cwagentlog.json -s
