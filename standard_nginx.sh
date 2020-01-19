7a157baf9967ae951e56654eafd811729c169c5f
praparn <eva10409@gmail.com>
2020-01-19 16:02:54 +0700

    20200119160253

---
+++ standard_nginx.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+#Set LC_ALL, LANG
+echo "LC_ALL=en_US.UTF-8" >> /etc/default/locate
+echo "LANG=en_US.UTF-8" >> /etc/default/locate
+
+#tuning sysctl.conf
+echo "net.core.rmem_max = 16777216" >> /etc/sysctl.conf
+echo "net.core.wmem_max = 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_rmem = 4096 87380 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_wmem = 4096 65536 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_probes = 9" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_time = 7200" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syn_retries = 5" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_no_metrics_save = 1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_synack_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syn_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_tw_reuse=1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_fin_timeout=15" >> /etc/sysctl.conf
+echo "net.ipv4.ip_local_port_range = 2000 65000" >> /etc/sysctl.conf
+echo "fs.file-max = 1000000" >> /etc/sysctl.conf
+echo "vm.swappiness = 0" >> /etc/sysctl.conf
+echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+#tuning limits.conf
+echo "* soft    nproc    65535" >> /etc/security/limits.conf
+echo "* hard    nproc    65535" >> /etc/security/limits.conf
+echo "* soft    nofile   65535" >> /etc/security/limits.conf
+echo "* hard    nofile   65535" >> /etc/security/limits.conf
+echo "root soft    nproc    65535" >> /etc/security/limits.conf  
+echo "root hard    nproc    65535" >> /etc/security/limits.conf
+echo "root soft    nofile   65535" >> /etc/security/limits.conf
+echo "root hard    nofile   65535" >> /etc/security/limits.conf
+
+#install NTP
+wget https://raw.githubusercontent.com/praparn/sourcesetup/master/standard_ntp.sh
+sudo chmod +x ./standard_ntp.sh
+./standard_ntp.sh
+
+#configure monitor memory on host
+apt-get update && apt-get install -y unzip libwww-perl libdatetime-perl
+mkdir /home/ubuntu/MonitoringMemoryandDisk
+cd /home/ubuntu/MonitoringMemoryandDisk
+curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
+unzip CloudWatchMonitoringScripts-1.2.2.zip && \
+rm CloudWatchMonitoringScripts-1.2.2.zip
+
+#add cronjob
+echo "*/5 * * * * /home/ubuntu/MonitoringMemoryandDisk/aws-scripts-mon/mon-put-instance-data.pl --mem-used-incl-cache-buff --mem-util --mem-used --mem-avail --disk-space-util --disk-path=/ --from-cron" | crontab -
+
+#install nginx
+deb http://nginx.org/packages/ubuntu/ xenial nginx
+deb-src http://nginx.org/packages/ubuntu/ xenial nginx
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $key
+apt-get update && apt-get install -y nginx
+
+#reboot
