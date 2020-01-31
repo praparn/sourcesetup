6dc12021170dbd0a8a6af2d59480c19233a66715
praparn <eva10409@gmail.com>
2020-01-31 21:23:22 +0700

    20200129190048

--- standard_terraform_aws.sh
+++ standard_terraform_aws.sh
@@ -34,5 +34,6 @@ echo "root hard    nofile   65535" >> /etc/security/limits.conf
 sudo apt-get update && sudo apt-get -y install python3-pip awscli unzip azure-cli
 pip3 install awscli --upgrade --user
 curl https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip > terraform_0.12.20_linux_amd64.zip && unzip terraform_0.12.20_linux_amd64.zip && sudo install terraform /usr/local/bin/
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 #restart
 #reboot
