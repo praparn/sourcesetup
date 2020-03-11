ba548b3c776b7a43e92b5bc63b02655d1d20b751
praparn <eva10409@gmail.com>
2020-03-11 20:34:36 +0700

    20200311203351

--- standard_kubernetes170_control.sh
+++ standard_kubernetes170_control.sh
@@ -71,7 +71,7 @@ systemctl daemon-reload
 systemctl restart docker
 
 #install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 #add ubuntu and 1001 to docker group
 usermod -a -G docker ubuntu
