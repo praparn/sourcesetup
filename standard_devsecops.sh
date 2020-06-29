d8ca63f1ed680edb6172a8093d834cdab6d08b6d
praparn <eva10409@gmail.com>
2020-06-29 22:33:01 +0700

    20200629202428

--- standard_devsecops.sh
+++ standard_devsecops.sh
@@ -1,12 +1,8 @@
 #Install Base Binary File and Initial Configuration on UltraVNC
 sudo apt update && apt install -y \
-  ubuntu-desktop \
+  xfce4 \
+  xfce4-goodies \
   vnc4server \
-  gnome-panel \
-  gnome-settings-daemon \
-  metacity \
-  nautilus \
-  gnome-terminal \
   ettercap-common \
   wireshark \
   nast \
@@ -15,4 +11,4 @@ sudo apt update && apt install -y \
   wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update && udo apt install code -y
+sudo apt update && sudo apt install code -y
