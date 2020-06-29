95f85a123dbbfc4c477269e8161cbad74e0ff1aa
praparn <eva10409@gmail.com>
2020-06-29 21:29:50 +0700

    20200629202428

--- standard_devsecops.sh
+++ standard_devsecops.sh
@@ -9,4 +9,10 @@ sudo apt update && apt install -y \
   gnome-terminal \
   ettercap-common \
   wireshark \
-  nast
+  nast \
+  software-properties-common \
+  apt-transport-https \
+  wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update && udo apt install code -y
