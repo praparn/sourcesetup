6ec79526aa81927bbc2ab70b29b395428001c8a6
praparn <eva10409@gmail.com>
2020-06-30 18:55:16 +0700

    20200629202428

--- standard_devsecops.sh
+++ standard_devsecops.sh
@@ -2,13 +2,13 @@
 sudo apt update && apt install -y \
   xfce4 \
   xfce4-goodies \
-  vnc4server \
+  tightvncserver \
   ettercap-common \
-  wireshark \
+  wireshark-gtk \
   nast \
   software-properties-common \
   apt-transport-https \
   wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update && sudo apt install code -y
+sudo apt update && udo apt install code -y
