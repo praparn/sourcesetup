ec2b0bd6c1bc91198899c211d2ef7a3d6efa78e6
praparn <eva10409@gmail.com>
2021-01-25 20:59:18 +0700

    20210125205654

--- standard_docker_20.sh
+++ standard_docker_20.sh
@@ -85,7 +85,7 @@ systemctl daemon-reload
 systemctl restart docker
 
 #install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.27.4/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 #add ubuntu and 1001 to docker group
 sudo usermod -a -G docker ubuntu
