a571bce2e62e4dde1027601c0211b4c4b8905624
Praparn Lungpoonlap <eva10409@gmail.com>
2021-04-23 00:09:10 +0700

    20210422235807

--- standard_host.sh
+++ standard_host.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-
 #Set LC_ALL, LANG
 echo "LC_ALL=en_US.UTF-8" >> /etc/default/locate
 echo "LANG=en_US.UTF-8" >> /etc/default/locate
+export LC_ALL=C
 
 #tuning sysctl.conf
 echo "net.core.rmem_max = 16777216" >> /etc/sysctl.conf
@@ -21,6 +21,22 @@ echo "net.ipv4.ip_local_port_range = 2000 65000" >> /etc/sysctl.conf
 echo "fs.file-max = 1000000" >> /etc/sysctl.conf
 echo "vm.swappiness = 0" >> /etc/sysctl.conf
 echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+#tuning limits.conf
+echo "* soft    nproc    65535" >> /etc/security/limits.conf
+echo "* hard    nproc    65535" >> /etc/security/limits.conf
+echo "* soft    nofile   65535" >> /etc/security/limits.conf
+echo "* hard    nofile   65535" >> /etc/security/limits.conf
+echo "root soft    nproc    65535" >> /etc/security/limits.conf  
+echo "root hard    nproc    65535" >> /etc/security/limits.conf
+echo "root soft    nofile   65535" >> /etc/security/limits.conf
+echo "root hard    nofile   65535" >> /etc/security/limits.conf
+
+sudo timedatectl set-timezone Asia/Bangkok
+sudo apt install -y chrony
+sudo sed -i -e 's/pool 2.debian.pool.ntp.org offline iburst/server 169.254.169.123 prefer iburst/g' /etc/chrony/chrony.conf
+sudo /etc/init.d/chrony restart
+#chronyc sources -v
+#restart
 
 #restart
 #reboot
