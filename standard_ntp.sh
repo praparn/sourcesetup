dff6f6e91ae1c27c60cd80f444f8a46418da4d6e
Praparn Lungpoonlap <eva10409@gmail.com>
2021-04-23 20:09:26 +0700

    20210423200923

--- standard_ntp.sh
+++ standard_ntp.sh
@@ -3,7 +3,10 @@
 #Set LC_ALL, LANG
 sudo timedatectl set-timezone Asia/Bangkok
 sudo apt install -y chrony
-sudo sed -i -e 's/pool 2.debian.pool.ntp.org offline iburst/server time.navy.mi.th prefer iburst/g' /etc/chrony/chrony.conf
+sudo sed -i -e 's/pool ntp.ubuntu.com        iburst maxsources 4//g' /etc/chrony/chrony.conf
+sudo sed -i -e 's/pool 0.ubuntu.pool.ntp.org iburst maxsources 1//g' /etc/chrony/chrony.conf
+sudo sed -i -e 's/pool 1.ubuntu.pool.ntp.org iburst maxsources 1//g' /etc/chrony/chrony.conf
+sudo sed -i -e 's/pool 2.ubuntu.pool.ntp.org iburst maxsources 2/server 169.254.169.123 prefer iburst minpoll 4 maxpoll 4/g' /etc/chrony/chrony.conf
 sudo /etc/init.d/chrony restart
 #chronyc sources -v
 #restart
