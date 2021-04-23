e67ff7bc8f57fe3db124793e0e031e67bacbb78a
Praparn Lungpoonlap <eva10409@gmail.com>
2021-04-23 19:52:40 +0700

    20210422235807

--- standard_host.sh
+++ standard_host.sh
@@ -33,7 +33,10 @@ echo "root hard    nofile   65535" >> /etc/security/limits.conf
 
 sudo timedatectl set-timezone Asia/Bangkok
 sudo apt install -y chrony
-sudo sed -i -e 's/pool 2.debian.pool.ntp.org offline iburst/server 169.254.169.123 prefer iburst/g' /etc/chrony/chrony.conf
+sudo sed -i -e 's/pool ntp.ubuntu.com        iburst maxsources 4//g' /etc/chrony/chrony.conf
+sudo sed -i -e 's/pool 0.ubuntu.pool.ntp.org iburst maxsources 1//g' /etc/chrony/chrony.conf
+sudo sed -i -e 's/pool 1.ubuntu.pool.ntp.org iburst maxsources 1//g' /etc/chrony/chrony.conf
+sudo sed -i -e 's/pool 2.ubuntu.pool.ntp.org iburst maxsources 2/server 169.254.169.123 prefer iburst minpoll 4 maxpoll 4/g' /etc/chrony/chrony.conf
 sudo /etc/init.d/chrony restart
 #chronyc sources -v
 #restart
