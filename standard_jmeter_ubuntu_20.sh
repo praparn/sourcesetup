88ff18b12ee58dcb3e28ce29eaaadb8029b46320
Praparn Lungpoonlap <eva10409@gmail.com>
2021-07-10 22:27:23 +0700

    20210710203537

--- standard_jmeter_ubuntu_20.sh
+++ standard_jmeter_ubuntu_20.sh
@@ -1,6 +1,7 @@
 #Install Base Binary File and Initial Configuration on UltraVNC
 sudo apt update && apt install -y \
   tigervnc-standalone-server \
-  ubuntu-gnome-desktop
+  ubuntu-gnome-desktop \
+  openjdk-8-jdk
 
 systemctl set-default multi-user.target
