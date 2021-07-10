76c9eff6e339b3b412acaf5d5d62c77eae2a7353
Praparn Lungpoonlap <eva10409@gmail.com>
2021-07-10 20:35:39 +0700

    20210710203537

---
+++ standard_jmeter_ubuntu_20.sh
@@ -0,0 +1,10 @@
+#Install Base Binary File and Initial Configuration on UltraVNC
+sudo apt update && apt install -y \
+  ubuntu-desktop \
+  vnc4server \
+  gnome-panel \
+  gnome-settings-daemon \
+  metacity \
+  nautilus \
+  gnome-terminal \
+  jmeter
