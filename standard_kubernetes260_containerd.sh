0796ab05e9335122d12f7c8186a8e16441aaac4c
Praparn Lungpoonlap <eva10409@gmail.com>
2023-03-05 14:43:20 +0700

    20230305144312

--- standard_kubernetes260_containerd.sh
+++ standard_kubernetes260_containerd.sh
@@ -60,7 +60,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update && apt-get install -y net-tools apt-transport-https ca-certificates curl gnupg software-properties-common ipvsadm lsb-release containerd.io
+apt-get update && apt-get install -y containerd.io
 rm /etc/containerd/config.toml
 
 
