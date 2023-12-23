dbe30d037f26bcc9a8b2a7d907378ae25df2db58
Praparn Lungpoonlap <eva10409@gmail.com>
2023-12-23 08:52:00 +0700

    update script setup k8s1.29

---
+++ standard_kubernetes290_containerd.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+#Set LC_ALL, LANG
+echo "LC_ALL=en_US.UTF-8" >> /etc/default/locate
+echo "LANG=en_US.UTF-8" >> /etc/default/locate
+
+#tuning sysctl.conf
+echo "net.core.rmem_max = 16777216" >> /etc/sysctl.conf
+echo "net.core.wmem_max = 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_rmem = 4096 87380 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_wmem = 4096 65536 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_probes = 9" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_time = 7200" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syn_retries = 5" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_no_metrics_save = 1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_synack_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syn_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_tw_reuse=1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_fin_timeout=15" >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+echo "net.ipv4.ip_local_port_range = 2000 65000" >> /etc/sysctl.conf
+echo "net.bridge.bridge-nf-call-iptables = 1" >> /etc/sysctl.conf
+echo "fs.file-max = 1000000" >> /etc/sysctl.conf
+echo "vm.swappiness = 0" >> /etc/sysctl.conf
+echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+echo "kernel.unprivileged_userns_clone = 0" >> /etc/sysctl.conf
+
+#tuning limits.conf
+echo "* soft    nproc    65535" >> /etc/security/limits.conf
+echo "* hard    nproc    65535" >> /etc/security/limits.conf
+echo "* soft    nofile   65535" >> /etc/security/limits.conf
+echo "* hard    nofile   65535" >> /etc/security/limits.conf
+echo "root soft    nproc    65535" >> /etc/security/limits.conf  
+echo "root hard    nproc    65535" >> /etc/security/limits.conf
+echo "root soft    nofile   65535" >> /etc/security/limits.conf
+echo "root hard    nofile   65535" >> /etc/security/limits.conf
+
+#set timezone bangkok
+timedatectl set-timezone Asia/Bangkok
+
+#configure prerequisites for enable module ipvs, overlay, netfilter
+echo "" > /etc/modules
+echo "ip_vs" >> /etc/modules
+echo "ip_vs_rr" >> /etc/modules
+echo "ip_vs_wrr" >> /etc/modules
+echo "ip_vs_sh" >> /etc/modules
+echo "nf_conntrack_ipv4" >> /etc/modules
+echo "overlay" >> /etc/modules
+echo "br_netfilter" >> /etc/modules
+
+#install containerd
+## Set up the repository:
+### Install packages to allow apt to use a repository over HTTPS
+
+apt-get update && apt-get install -y ca-certificates curl gnupg lsb-release
+curl -sL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-keyring.gpg
+add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable"
+#mkdir -m 0755 -p /etc/apt/keyrings (for ubuntu 20.04)
+apt update && apt -y install containerd.io net-tools
+containerd config default                              \
+ | sed 's/SystemdCgroup = false/SystemdCgroup = true/' \
+ | sudo tee /etc/containerd/config.toml
+systemctl restart containerd
+
+#Install Kubernetes Base
+curl -sLo /etc/apt/trusted.gpg.d/kubernetes-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
+apt-add-repository -y "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt-get update && apt-get install -y kubectl=1.29.0-00 kubelet=1.29.0-00 kubeadm=1.29.0-00 && apt-mark hold kubelet kubeadm kubectl
+#restart
+sudo shutdown -r now
+#reboot
