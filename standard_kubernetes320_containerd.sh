59cfbf2b0aa737579331c792f09234e5342aa9d1
Praparn Lungpoonlap <eva10409@gmail.com>
2025-03-08 10:12:01 +0700

    update file

--- standard_kubernetes320_containerd.sh
+++ standard_kubernetes320_containerd.sh
@@ -53,6 +53,7 @@ echo "br_netfilter" >> /etc/modules
 ### Install packages to allow apt to use a repository over HTTPS
 
 apt-get update && apt-get install -y ca-certificates curl gnupg lsb-release apt-transport-https
+
 curl -sL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-keyring.gpg
 add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu jammy stable"
 #mkdir -m 0755 -p /etc/apt/keyrings (for ubuntu 20.04)
@@ -64,11 +65,13 @@ systemctl restart containerd
 
 #Install Kubernetes Base
 mkdir -m 755 /etc/apt/keyrings #special for debian 12 and ubuntu 22.04
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
 #curl -sLo /etc/apt/trusted.gpg.d/kubernetes-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
 #apt-add-repository -y "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-apt-get update && apt-get install -y kubectl=1.30.0-1.1 kubelet=1.30.0-1.1 kubeadm=1.30.0-1.1 && apt-mark hold kubelet kubeadm kubectl
+apt-get update && apt-get install -y kubectl kubelet kubeadm && apt-mark hold kubelet kubeadm kubectl
 #restart
 sudo shutdown -r now
 #reboot
